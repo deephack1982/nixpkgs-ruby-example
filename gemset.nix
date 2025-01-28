7d3af757e22f091595425e2c736e6eed6e31ae62
Mark Dickie <mark.dickie@gmail.com>
2025-01-28 16:30:31 +0100

    Initial commit

---
+++ gemset.nix
@@ -0,0 +1,12 @@
+{
+  mikrotik_config = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0kwa4f95qfk389c8yl4s9ci9miydz8fbzl2a14m2dp9hmf8fk889";
+      type = "gem";
+    };
+    version = "0.0.1";
+  };
+}
